16f36011f9bf2b18294ab1faf9c15e5edd2f8c5e
Kumaraswamy B G <71964026+XomaDev@users.noreply.github.com>
2021-03-12 19:30:58 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var isRedy = true;
+
+async function processRequest() {
+    var button = document.getElementById('submitBtn');
+
+    if(isRedy && button.innerHTML == 'Shorten URL') {
+        isRedy = false;
+
+        button.innerHTML = 'Processing';
+
+        var textBox = document.getElementById("linkInput");
+        var requestURL = "https://sapi.peico.xyz/api/add?URL=" + textBox.value;
+
+        console.log("GET " + requestURL);
+
+        fetch(requestURL).then(function(response){
+            response.text().then(function(data) {
+                isRedy = true;
+                button.innerHTML = 'Shorten URL';
+                textBox.value = "peico.xyz/" + data;;
+            });
+            }).catch(function(error) {
+                button.innerHTML = 'Error';
+        });
+    }
+}
+
+function openApp() {
+    if(Math.random().toString(2).substr(2, 1) == 1) {
+        window.open("https://play.google.com/store/apps/details?id=xyz.peico.app");
+     } else {
+        window.open("https://play.google.com/store/apps/details?id=appinventor.ai_bgpsdr.Peico");
+     }
+}
